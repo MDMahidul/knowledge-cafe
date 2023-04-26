29323e0a99c8756d1977215c26b3d013b07e446c
MDMahidul <mmbmahidul007@gmail.com>
2023-04-26 08:02:33 +0600

    created react app and install all the dependencies

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-success'>Check</h1>
+    </>
+  )
+}
+
+export default App
